e191b962e1f70d00eb945e563c8a08290e497e6a
asus <asus@DESKTOP-NA4TQEI>
2020-02-09 16:22:53 +0300

    0.1

--- Playground/src/mainPlayground/Main.java
+++ Playground/src/mainPlayground/Main.java
@@ -3,13 +3,14 @@ package mainPlayground;
 import java.util.Arrays;
 
 import sorts.AdvancedSorts;
+import sorts.PrimitiveSorts;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integer[] arr = {3,4,2,1,7,6,9,0,-1};
-		AdvancedSorts.heapSort(arr);
+		PrimitiveSorts.bubbleSort((arr));
 		System.out.println(Arrays.toString(arr));
 	}
 }
