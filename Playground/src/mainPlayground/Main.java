fe8e2186a956c01de3e15a2fd816027e985c4834
Sarp Sümer <ssuemer@student-net.ethz.ch>
2020-02-14 17:55:28 +0300

    ..

--- Playground/src/mainPlayground/Main.java
+++ Playground/src/mainPlayground/Main.java
@@ -1,19 +1,11 @@
 package mainPlayground;
 
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.LinkedList;
-
-import graphstuff.GraphReader;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		LinkedList<Integer>[] adjlist = GraphReader.readIntoList("files\\graph.txt");
-		for (LinkedList<Integer> linkedList : adjlist) {
-			System.out.println(linkedList.toString());
-		}
 	}	
 }
 
