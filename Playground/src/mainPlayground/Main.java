49385754392c9d3169f91b6f9943787e2a236526
Sarp Sümer <ssuemer@student-net.ethz.ch>
2020-07-11 10:08:43 +0300

    11.7

--- Playground/src/mainPlayground/Main.java
+++ Playground/src/mainPlayground/Main.java
@@ -7,14 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		LinkedList<Integer> l = new LinkedList<Integer>();
-		l.add(1);
-		l.add(2);
-		LinkedList<Integer> k = new LinkedList<Integer>();
-		k.add(4);
-		k.add(9);
-		l.addAll(1, k);
-		System.out.println(l);
+		System.out.println(Runtime.getRuntime().availableProcessors());
 	}
 	
 	
