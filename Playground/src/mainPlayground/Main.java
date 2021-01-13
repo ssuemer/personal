6de175437f87aa006c0235ad2ac2e82a4afb53dc
Sarp Sümer <ssuemer@student-net.ethz.ch>
2021-01-13 22:22:37 +0300

    13.01.2021

--- Playground/src/mainPlayground/Main.java
+++ Playground/src/mainPlayground/Main.java
@@ -1,18 +1,12 @@
 package mainPlayground;
-
-
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class Main {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		ArrayList<Integer> a = new ArrayList<Integer>();
-		a.add(3);
-		a.add(34);
-		a.add(335);
-		System.out.println(a);
+		String s = "xsa";
+		System.out.println(s);
 	}
 }
 
