0afc426af111ca1d88b6838b8fd5c01587330440
asus <asus@DESKTOP-NA4TQEI>
2021-12-11 21:45:38 +0100

    Modification test

--- Playground/src/misc/Main.java
+++ Playground/src/misc/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		HanoiVisual h = new HanoiVisual(15);
-		h.setPace(0);
-		h.execute();
+		System.out.println("Modified on laptop");
 	}
 
 }
